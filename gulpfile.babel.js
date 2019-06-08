34956fa2bf0c5da6e9209f8f057e8c027fad23ef
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-06-08 19:09:34 +0900

    3.1.1 gulp-csso: minifyCSS & watch

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,19 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import minifyCSS from 'gulp-csso';
 
 sass.compiler = require('node-sass');
 
 const paths = {
 	styles: {
 		src: 'assets/scss/styles.scss',
-		dest: 'src/static/styles'
+		dest: 'src/static/styles',
+		watch: 'assets/scss/**/*.scss'
 	}
 };
 
-export function styles() {
+function styles() {
 	return gulp
 		.src(paths.styles.src)
 		.pipe(sass())
@@ -20,5 +22,14 @@ export function styles() {
 				cascade: false
 			})
 		)
+		.pipe(minifyCSS())
 		.pipe(gulp.dest(paths.styles.dest));
 }
+
+function watchFiles() {
+	gulp.watch(paths.styles.watch, styles);
+}
+
+const dev = gulp.series([ styles, watchFiles ]);
+
+export default dev;
