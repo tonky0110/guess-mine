92d7bf9595057551b3e4189e36aa22ef8652e6cf
tonky0110 <tonky0110@tonky0110ui-MacBookPro.local>
2019-06-08 11:00:32 +0900

    2.0.1 add morgan(logger) and setup

--- src/server.js
+++ src/server.js
@@ -1,16 +1,20 @@
 import { join } from 'path';
 import express from 'express';
 import socketIO from 'socket.io';
+import logger from 'morgan';
 
 const PORT = 4000;
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', join(__dirname, 'views'));
 app.use(express.static(join(__dirname, 'static')));
+app.use(logger('dev'));
 app.get('/', (req, res) => res.render('home'));
 
 const handleListening = () => console.log(`✅ Server running: http://localhost:${PORT}`);
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = socketIO.listen(server);
+
+io.on('connection', () => console.log('Somebody connected'));
